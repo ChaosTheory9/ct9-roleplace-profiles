a521027968eec0940d0ef0905b47bc4e9b45c6f9
ChaosTheory9 <rp.chaostheory@gmail.com>
2020-08-08 12:41:15 -0400

    commented out the thing that doesn't work

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+/*
 var profileContent = document.createElement('div'); 
  
 profileContent.innerHTML = 'https://chaostheory9.github.io/ct9-roleplace-profiles/profile.html'; 
 document.getElementsByTagName('body')[0].appendChild(profileContent); 
+*/
