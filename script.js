56ee921bf73cde0ee5fc3d0b14951493384adef1
ChaosTheory9 <rp.chaostheory@gmail.com>
2020-08-08 12:24:33 -0400

    Added a possible html inject script?

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+var profileContent = document.createElement('div'); 
+ 
+profileContent.innerHTML = 'url-to-gitpage-html-file'; 
+document.getElementsByTagName('body')[0].appendChild(profileContent); 
