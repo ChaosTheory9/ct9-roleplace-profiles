d43f6f2afc2dfddd8611ad327d1b5678e877f9db
ChaosTheory9 <rp.chaostheory@gmail.com>
2020-08-08 13:33:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 function includeHTML() {
-  var parentPage, i, elmnt, includeFilePath, xhttp;
-  /* Loop through a collection of all HTML elements: */
-  parentPage = document.getElementsByTagName("*");
-  for (i = 0; i < parentPage.length; i++) {
-    elmnt = parentPage[i];
-    /*search for elements with a certain atrribute:*/
-    includeFilePath = elmnt.getAttribute("data-include-html");
-    if (includeFilePath) {
-      /* Make an HTTP request using the attribute value as the file name: */
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /* Remove the attribute */
-          elmnt.removeAttribute("data-include-html");
-          //includeHTML();
+  var parentPage, elmnt, includeFilePath, xhttp;
+  parentPage = document.getElementsByTagName("body");
+  elmnt = parentPage[0];
+  /*check if the body has the correct attribute:*/
+  includeFilePath = elmnt.getAttribute("data-include-html");
+  if (includeFilePath) {
+    /* Make an HTTP request using the attribute value as the file name: */
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4) {
+        if (this.status == 200) {
+          elmnt.innerHTML = this.responseText;
+        } else if (this.status == 404) {
+          elmnt.innerHTML = "Page not found.";
         }
+        /* Remove the attribute */
+        elmnt.removeAttribute("data-include-html");
+        //includeHTML();
       }
-      xhttp.open("GET", includeFilePath, true);
-      xhttp.send();
-      /* Exit the function: */
-      return;
     }
+    xhttp.open("GET", includeFilePath, true);
+    xhttp.send();
+    /* Exit the function: */
+    return;
   }
-} //end includeHTML()
+}
