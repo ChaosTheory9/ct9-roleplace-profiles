f13e68d53c929256b5bfbbb9548969c827218dfd
ChaosTheory9 <rp.chaostheory@gmail.com>
2020-08-08 13:20:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-/*
-var profileContent = document.createElement('div'); 
- 
-profileContent.innerHTML = 'https://chaostheory9.github.io/ct9-roleplace-profiles/profile.html'; 
-document.getElementsByTagName('body')[0].appendChild(profileContent); 
-*/
+function includeHTML() {
+  var parentPage, i, elmnt, includeFilePath, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  parentPage = document.getElementsByTagName("*");
+  for (i = 0; i < parentPage.length; i++) {
+    elmnt = parentPage[i];
+    /*search for elements with a certain atrribute:*/
+    includeFilePath = elmnt.getAttribute("data-include-html");
+    if (includeFilePath) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute */
+          elmnt.removeAttribute("data-include-html");
+          //includeHTML();
+        }
+      }
+      xhttp.open("GET", includeFilePath, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+} //end includeHTML()
