c03121f2b842bffa277b85fb71f2f7195005f1b2
ChaosTheory9 <rp.chaostheory@gmail.com>
2020-08-13 19:11:11 -0400

    Update tabPanelsScript.js

--- tabPanelsScript.js
+++ tabPanelsScript.js
@@ -14,3 +14,21 @@ function showPanel(panelIndex, colorCode = '#ffffff') {
     tabPanels[panelIndex].style.backgroundColor=colorCode;
 }
 showPanel(0,'#f44336');
+/*
+    SAMPLE HTML
+    
+    <div class="tabContainer">
+        <div class="buttonContainer">
+            <button onclick="showPanel(0)">Tab 1</button>
+            <button onclick="showPanel(1,'#4caf50')">Tab 2</button>
+            <button onclick="showPanel(2,'#2196f3')">Tab 3</button>
+            <button onclick="showPanel(3,'#ff5722')">Tab 4</button>
+        </div>
+        <div class="tabPanel">Tab 1:Content</div>
+        <div class="tabPanel">Tab 2:Content</div>
+        <div class="tabPanel">Tab 3:Content</div>
+        <div class="tabPanel">Tab 4:Content</div>
+    </div>
+*/
+
+// The color parameter is completely optional when you call the event function. By default, it makes the color white. I think you could use 'inherit' for that too but I'm not sure.
